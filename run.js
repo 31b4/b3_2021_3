f5d3a6a67998fa5bf38322f5256f628cd3a3ee3e
31b4 <75566095+31b4@users.noreply.github.com>
2021-10-25 21:06:50 +0200

    Add files via upload

--- run.js
+++ run.js
@@ -1,20 +1,51 @@
-function KepGen() {
+var kepXPos;
+var kepYPos;
+
+function VeletlenSzam(params) {
+    return Math.floor(Math.random()*params+5)
+}
+function KepGen(ctx) {
+    kepXPos = VeletlenSzam(895)
+    kepYPos = VeletlenSzam(595)
+    var img = new Image();
+    img.src = 'img/2.png';                    
+    ctx.drawImage(img, kepXPos, kepYPos,100,100);
+}
+
+
+function Main() {
     var c = document.getElementById("canvas");
     var ctx = c.getContext("2d");
-    var img = new Image();
-    img.src = 'img/2.png';
-    img.width = "100px";
-    ctx.drawImage(img,10,10,100,100);
+    ctx.clearRect(0,0,c.offsetWidth,c.offsetHeight); 
+    
+    /*
+        for (let i = 0; i < 5; i++) {
+            KepGen(ctx);
+        }
+    */
+    
+   KepGen(ctx);   
+   
+   
+   var myVar = setInterval(document.getElementById("elsoSzint").addEventListener('click', clickEvent(), false) ,30);
 }
 
-function clickEvent(event){
-    var c = document.getElementById("myCanvas");
+function clickEvent(){
+    var c = document.getElementById("canvas");
+    var ctx = c.getContext("2d");
     var cavasRect = c.getBoundingClientRect();
-    var xPos = event.clientX - cavasRect.left;
-    var yPos = event.clientY - cavasRect.top;
-    ballObjects.push(new Ball(xPos, yPos, possibleColors[Math.floor(Math.random()*possibleColors.length)], 3+Math.floor(Math.random()*12)));
+    var xPos = event.clientX;
+    var yPos = event.clientY;
+    console.log(xPos,yPos)
+    if (xPos > kepXPos  && xPos < kepXPos+100) {
+        if (yPos > kepYPos && yPos < kepYPos+100) {
+            ctx.clearRect(xPos, yPos, 100, 100);
+            console.log("itt vagyok");
+            console.log(kepXPos, kepYPos);
+        }
+    }
 }
+		
 
-document.getElementById("canvas").addEventListener('click', clickEvent,
-false);
+
 
