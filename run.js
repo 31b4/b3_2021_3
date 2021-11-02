be03937d8cba2cee63c7d11be66acb122bfb99a8
31b4 <75566095+31b4@users.noreply.github.com>
2021-11-02 14:52:41 +0100

    Add files via upload

--- run.js
+++ run.js
@@ -10,10 +10,11 @@ function KepGen(ctx) {
     var img = new Image();
     img.src = 'img/2.png';                    
     ctx.drawImage(img, kepXPos, kepYPos,100,100);
+    console.log("kep x: "+ kepXPos + " kep y: " + kepYPos)
 }
 
 
-function Main() {
+function Main() {// miert kell 2x ranyomni es miert nem jelenik meg neha (bugos)
     var c = document.getElementById("canvas");
     var ctx = c.getContext("2d");
     ctx.clearRect(0,0,c.offsetWidth,c.offsetHeight); 
@@ -24,28 +25,45 @@ function Main() {
         }
     */
     
-   KepGen(ctx);   
+    KepGen(ctx);   
    
-   
-   var myVar = setInterval(document.getElementById("elsoSzint").addEventListener('click', clickEvent(), false) ,30);
 }
-
-function clickEvent(){
-    var c = document.getElementById("canvas");
-    var ctx = c.getContext("2d");
-    var cavasRect = c.getBoundingClientRect();
-    var xPos = event.clientX;
-    var yPos = event.clientY;
-    console.log(xPos,yPos)
-    if (xPos > kepXPos  && xPos < kepXPos+100) {
-        if (yPos > kepYPos && yPos < kepYPos+100) {
-            ctx.clearRect(xPos, yPos, 100, 100);
-            console.log("itt vagyok");
-            console.log(kepXPos, kepYPos);
+function VanEOttKep(x,y){
+    var c = document.getElementById("canvas"); // kell e itt létrehozni még egyszer a c-t és a ctx-et vagy meghívjuk inkabb
+    var ctx = c.getContext("2d"); 
+    if (x >= kepXPos  && x <= kepXPos+100) {
+        if (y >= kepYPos && y <= kepYPos+100) {
+            ctx.clearRect(kepXPos, kepYPos, 100, 100);
+            console.log("itt vagyok");            
         }
     }
 }
-		
+document.addEventListener("DOMContentLoaded", init, false);
 
+function init() {
+    var canvas = document.getElementById("canvas");
+    canvas.addEventListener("mousedown", getPosition, false);
+}
 
-
+function getPosition(event) {
+    var x = new Number();
+    var y = new Number();
+    var canvas = document.getElementById("canvas");
+    if (event.x != undefined && event.y != undefined) {
+        x = event.x;
+        y = event.y;
+    }   
+    else {// Firefox method to get the position
+        x = event.clientX + document.body.scrollLeft +
+            document.documentElement.scrollLeft;
+        y = event.clientY + document.body.scrollTop +
+            document.documentElement.scrollTop;
+    }
+    x -= canvas.offsetLeft;
+    y -= canvas.offsetTop;
+    //alert("x: " + x + "  y: " + y);
+    console.clear()
+    console.log("x: " + x + "  y: " + y)
+    console.log("kep x: "+ kepXPos + " kep y: " + kepYPos)
+    VanEOttKep(x,y);
+}
